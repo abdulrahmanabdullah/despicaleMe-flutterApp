1adf1163e3c639f98b3b16168400e4471fcceda6
abdulrahmanabdullah <nfs056@gmail.com>
2019-07-23 07:38:54 +0300

    change background color when user select specific colors in character_details file

--- lib/screens/characters_details.dart
+++ lib/screens/characters_details.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:after_layout/after_layout.dart';
 import '../model/characters_model.dart';
 import '../utlities/app_theme.dart';
 
 class CharactersDetails extends StatefulWidget {
   final Character charater;
+
+  // just hide colors in clips without clips word .
+  final double _collapsedBottomSheetBottom = -250;
+
+  // to show colors in clips
+  final double _exapendBottomSheetBottom = 0;
+
+  // to hidden clips completely
+  // this value become like 330 because container height = 80 and clips = 250 then 80 + 250 = 330
+  final double _completeCollapsedBottomSheet = -330;
 
   CharactersDetails({Key key, this.charater}) : super(key: key);
 
@@ -11,27 +22,74 @@ class CharactersDetails extends StatefulWidget {
   _CharactersDetailsState createState() => _CharactersDetailsState();
 }
 
-class _CharactersDetailsState extends State<CharactersDetails> {
+class _CharactersDetailsState extends State<CharactersDetails>
+    with AfterLayoutMixin<CharactersDetails> {
+  double _currentBottomSheetPosition = -330;
+
+  bool isCollpased = false;
+
+  List<Color> currentColor;
+
+  @override
+  void initState() {
+    super.initState();
+    currentColor = List.from(widget.charater.color);
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: InkWell(
-        onTap: () {
-          _closePageEventListener();
-        },
-        child: Stack(fit: StackFit.expand, children: <Widget>[
-          DecoratedBox(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: currentColor.last,
+              gradient: LinearGradient(
+            colors: currentColor,
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          )),
+        ),
+        _layoutPage(screenHeight),
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.bounceOut,
+          bottom: _currentBottomSheetPosition,
+          left: 0,
+          right: 0,
+          child: Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-              colors: widget.charater.color,
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-            )),
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                InkWell(
+                  onTap: _closeClipsColors,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 32),
+                    height: 80,
+                    child: Text(
+                      "Clips",
+                      style: UtlitesTextStyle.subHeading
+                          .copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: _clpisWidget(),
+                ),
+              ],
+            ),
           ),
-          _layoutPage(screenHeight),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 
@@ -126,5 +184,108 @@ class _CharactersDetailsState extends State<CharactersDetails> {
         ),
       ),
     );
+  }
+
+  Widget _clpisWidget() {
+    return Container(
+      height: 250,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      //First Row
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.deepOrange),
+              SizedBox(height: 20),
+              _fillClipsColor(Colors.orange),
+            ],
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.redAccent),
+              SizedBox(height: 20),
+              _fillClipsColor(Colors.deepPurple),
+            ],
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.blue),
+              SizedBox(height: 20),
+              _fillClipsColor(Colors.lightBlue),
+            ],
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.black54),
+               SizedBox(height: 20),
+              _fillClipsColor(Colors.black26),
+            ],
+          ),
+          SizedBox(width: 12,),
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.purple),
+              SizedBox(height: 20),
+              _fillClipsColor(Colors.deepOrangeAccent),
+            ],
+          ),
+          SizedBox(width: 12,),
+          Column(
+            children: <Widget>[
+              _fillClipsColor(Colors.black54),
+              SizedBox(height: 20),
+              _fillClipsColor(Colors.black38),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Fill color of each box inside the clips widget.
+  Widget _fillClipsColor(Color color) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          currentColor.clear();
+          currentColor.addAll([color,color]);
+          _closeClipsColors();
+        });
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.all(Radius.circular(20))),
+      ),
+    );
+  }
+
+  _closeClipsColors() {
+    setState(() {
+      _currentBottomSheetPosition = isCollpased
+          ? widget._exapendBottomSheetBottom
+          : widget._collapsedBottomSheetBottom;
+      isCollpased = !isCollpased;
+    });
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        isCollpased = true;
+        _currentBottomSheetPosition = widget._collapsedBottomSheetBottom;
+      });
+    });
   }
 }
